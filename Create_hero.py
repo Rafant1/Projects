483ff39bdd83e4f5ca450a824c23fb082756f644
Rafał Antczak <rantczak112@wp.pl>
2020-11-10 19:47:48 +0100

    fixed(changed) some functions names

--- Create_hero.py
+++ Create_hero.py
@@ -12,33 +12,34 @@ class Hero():
         self.magic_attack_points = Hmagic_attack_points
         self.luck = Hluck
 
-    def getHealth(self):
+    def get_health(self):
         return self.health
-    def getAttack(self):
+    def get_attack(self):
         return self.attack_points
-    def getDefense(self):
+    def get_defense(self):
         return self.defense_points
-    def getRangeAttack(self):
+    def get_range_attack(self):
         return self.range_attack_points
-    def getMagicAttack(self):
+    def get_magic_attack(self):
         return self.magic_attack_points
-    def getLuck(self):
+    def get_luck(self):
         return self.luck
 
 
-    def setHealth(self, newHealth):
-        self.health = newHealth
-    def setAttack(self, newAttack):
-        self.attack_points = newAttack
-    def setDefense(self, newDefense):
-        self.defense_points = newDefense
-    def setRangeAttack(self, newRange):
-        self.range_attack_points = newRange
-    def setMagicAttack(self, newMagic):
-        self.magic_attack_points = newMagic
+    def set_health(self, new_health):
+        self.health = new_health
+    def set_attack(self, new_attack):
+        self.attack_points = new_attack
+    def set_defense(self, new_defense):
+        self.defense_points = new_defense
+    def set_range_attack(self, new_range):
+        self.range_attack_points = new_range
+    def set_magic_attack(self, new_magic):
+        self.magic_attack_points = new_magic
 
 
-def Create_your_class():
+def create_your_class():
+#From this function the class Hero will take the main arguments
 
     name = input("What name you would like to chose? ")
     print(f"Hello, {name}.")
@@ -94,7 +95,7 @@ Mage(4)
 
 
 
-Hero_attributes = Create_your_class()
+Hero_attributes = create_your_class()
 Hero1 = Hero(Hero_attributes[0], Hero_attributes[1], Hero_attributes[2], Hero_attributes[3], Hero_attributes[4], Hero_attributes[5], Hero_attributes[6])
 print(f"The class you selected is {Hero_attributes[7]}. Those are your attributes:")
 print(f"name: {Hero1.name}")

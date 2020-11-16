83d7d52487fa357f61cf7ac833bb0c367d7d2c73
Rafał Antczak <rantczak112@wp.pl>
2020-11-17 00:41:46 +0100

    created a if statement which limites the minimum health points value to 0 points in player and monster attack functions

--- battle_functions.py
+++ battle_functions.py
@@ -15,11 +15,16 @@ def enemy_dead(enemy):
 def enemy_attack(hero, enemy):
     enemy_attack_multiplicator = random.randint(1,3)
     chance_attack = random.randint(1,10)
+
     if chance_attack >= 6:
         hero.set_health(hero.get_health() - (enemy.get_attack() * enemy_attack_multiplicator))
         print("It is the enemy turn...")
         print(f"The enemy dealt you {enemy.get_attack() * enemy_attack_multiplicator} damage points.")
-        print(f"Your actual health is {hero.get_health()} health points.\n")
+        if hero.get_health() > 0:
+            print(f"Your actual health is {hero.get_health()} health points.\n")
+        else:
+            print("Your actual health is 0 points.")
+
     else:
         print("The enemy tried to hit you, but it missed.")
         print(f"Your actual health is {hero.get_health()} health points.\n")
@@ -34,11 +39,15 @@ def player_attack(hero, enemy):
 ''')
     if player_attack_type == "1":
         chance_attack = random.randint(1, 10)
+
         if chance_attack >= 6:
             enemy.set_health(enemy.get_health() - hero.get_attack() - enemy.get_normal_attack_vulnability())
             print("You swing the sword...")
             print(f"You dealt {hero.get_attack() + enemy.get_normal_attack_vulnability()} damage points.")
-            print(f"The actual enemy health is {enemy.get_health()}.\n")
+            if enemy.get_health() > 0:
+                print(f"The actual enemy health is {enemy.get_health()}.\n")
+            else:
+                print(f"The actual enemy health is 0 points.")
 
         else:
             print("You swing the sword...")
@@ -51,7 +60,10 @@ def player_attack(hero, enemy):
             enemy.set_health(enemy.get_health() - hero.get_range_attack() - enemy.get_range_attack_vulnability())
             print("You draw a bow ...")
             print(f"You dealt {hero.get_range_attack() + enemy.get_range_attack_vulnability()} damage points.")
-            print(f"The actual enemy health is {enemy.get_health()}.\n")
+            if enemy.get_health() > 0:
+                print(f"The actual enemy health is {enemy.get_health()}.\n")
+            else:
+                print(f"The actual enemy health is 0 points.")
 
         else:
             print("You draw a bow...")
@@ -64,7 +76,10 @@ def player_attack(hero, enemy):
             enemy.set_health(enemy.get_health() - hero.get_magic_attack() - enemy.get_magic_attack_vulnability())
             print("You cast a spell...")
             print(f"You dealt {hero.get_magic_attack() + enemy.get_magic_attack_vulnability()} damage points.")
-            print(f"The actual enemy health is {enemy.get_health()}.\n")
+            if enemy.get_health() > 0:
+                print(f"The actual enemy health is {enemy.get_health()}.\n")
+            else:
+                print(f"The actual enemy health is 0 points.")
 
         else:
             print("You cast a spell...")
